6044007d8c73d0d4c54d87f53cb6459ef9f99975
KobyCanesa <59832967+kobycanesa@users.noreply.github.com>
2020-03-16 12:29:23 +0000

    r1
    Signed-off-by: KobyCanesa <kobyanesa@gmail.com>

--- coursework.java
+++ coursework.java
@@ -1,4 +1,3 @@
-#include <stdio.h>
 import world.sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,5 +8,108 @@ public class coursework {
     public static void main(String[] args) {
 
         }
+
+    public CountryRecord getCountry(int Code)
+
+    {
+
+        try
+
+        {
+
+            //SQL Statement
+
+            Statement stmt = con.createStatement();
+
+            String strSelect = "SELECT Code, Name, Continent, Region, SurfaceArea, IndepYear, Population, LifeExpectacy, GNP, GNPOLd, LocalName, GovernmentForm, HeadOfState, Capital, Code2 "
+
+                            + "FROM Country "
+
+                            + "ORDERBY Population DESC;
+
+            // Runs the Statement
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Brings the country/countries found to the screen
+
+            // Check if a country can be found
+
+            if (rset.next())
+
+            {
+
+                CountryRecord cou = new CountryRecord();
+
+                cou.Code = rset.getInt("Code");
+
+                cou.Name = rset.getString("Name");
+
+                cou.Continent = rset.getString("Continent");
+
+                cou.Region = rset.getString("Region");
+
+                cou.SurfaceArea = rset.getString("SurfaceArea");
+
+                cou.IndepYear = rset.getString("IndepYear");
+
+                cou.Population = rset.getString("Population");
+
+                cou.LifeExpectacy = rset.getString("LifeExpectacy");
+
+                cou.GNP = rset.getString("GNP");
+
+                cou.GNPOLd = rset.getString("GNPOLd");
+
+                cou.LocalName = rset.getString("LocalName");
+
+                cou.GovernmentForm = rset.getString("GovernmentForm");
+
+                cou.HeadOfState = rset.getString("HeadOfState");
+
+                cou.Capital = rset.getString("Capital");
+
+                cou.Code2 = rset.getString("Code2");
+
+                return cou;
+
+            }
+
+            else
+
+                //If one can't be found return nothing
+
+                return null;
+
+        }
+
+        //Throws a message if a error is found
+
+        catch (Exception e)
+
+        {
+
+            System.out.println(e.getMessage());
+
+            System.out.println("Failed to get Country details");
+
+            return null;
+
+        }
+
+       //if cou is not blank then it will bring all the information to the screen in one line for each country
+
+        if (cou != null)
+        {
+            System.out.println(
+                    Cou.Code + " "
+                            + cou.Name + " "
+                            + cou.Continent + " "
+                            + cou.Region + " "
+                            + cou.SurfaceArea + " "
+                    + cou.IndepYear + " "
+                    + "Manager: " + cou.Population+ " " + cou.LifeExpectacy + " " + cou.GNP + " " + GNPOld + " " + LocalName + " " + GovernmentForm + " " + cou.HeadOfState + " " cou.capital + " " + cou.Code2);
+        }
+
     }
 }
---
+++ test/java/MyTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyTest
+{
+
+    public int method(String cou)
+    {
+        if (cou != null)
+            return cou.Name.length();
+        else
+            return -1;
+    }
+
+    public int method(String cou)
+    {
+        if (cou != null)
+            return cou.Code.length();
+        else
+            return -1;
+    }
+
+    public int method(String cou)
+    {
+        if (cou != null)
+            return cou.Continent.length();
+        else
+            return -1;
+    }
+
+    public int method(String cou)
+    {
+        if (cou != null)
+            return cou.Region.length();
+        else
+            return -1;
+    }
+
+    public int method(String cou)
+    {
+        if (cou != null)
+            return cou.Population.length();0
+        else
+            return -1;
+    }
+
+}
